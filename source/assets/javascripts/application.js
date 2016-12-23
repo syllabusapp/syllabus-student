69d69bd81a60e10d1c00cced186f38e948d53258
John Wilson <john@j-wilson.com>
2016-12-23 15:02:43 -0500

    Get react working and show logo

--- source/assets/javascripts/application.js
+++ source/assets/javascripts/application.js
@@ -1,1 +1,19 @@
-console.log("application.js loaded successfully");
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class Index extends React.Component {
+  render() {
+    return (
+      <div className="cell well">
+        <a href="#">
+          <img src="/assets/images/logo.svg" />
+        </a>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <Index />,
+  document.getElementById("app")
+)
--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,18 @@ module.exports = {
   entry: './source/assets/javascripts/application.js',
   output: {
     path: '/build',
-    filename: 'application.js',
-    libraryTarget: 'var',
-    library: 'NV'
+    filename: 'application.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
   }
 };
