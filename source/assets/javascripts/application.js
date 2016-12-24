4da27ec90ed446922ac1917eca052fcdf7e1ebbb
John Wilson <john@j-wilson.com>
2016-12-23 19:41:23 -0500

    Add more placeholder content to main content

--- source/assets/javascripts/application.js
+++ source/assets/javascripts/application.js
@@ -2,13 +2,32 @@ import CourseNavigation from "./components/CourseNavigation"
 import React from "react"
 import ReactDOM from "react-dom"
 
-class Index extends React.Component {
+class AppFrame extends React.Component {
   render() {
-    return <CourseNavigation />;
+    return (
+      <div className="row">
+        <div className="cell">
+          <CourseNavigation />
+          <div className="g">
+            <aside className="g-b g-b--3of12">
+              <nav>
+                <a href="#">Courses</a>
+                <br />
+                <a href="#">Documents</a>
+              </nav>
+            </aside>
+            <main className="g-b g-b--9of12">
+              <h2 className="heading heading--s">My Current Courses</h2>
+              <h2 className="heading heading--s">Upcoming Courses</h2>
+            </main>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
 ReactDOM.render(
-  <Index />,
+  <AppFrame />,
   document.getElementById("app")
 )
