716eece45496ac21ee3a02b93c8dab11793a2a40
John Wilson <john@j-wilson.com>
2016-12-24 14:40:27 -0500

    Add items to index page

--- source/assets/javascripts/application.js
+++ source/assets/javascripts/application.js
@@ -28,7 +28,36 @@ class AppFrame extends React.Component {
             </aside>
             <main className="g-b g-b--9of12">
               <h2 className="heading heading--s">My Current Courses</h2>
+              <ul className="has-items">
+                <li>
+                  <a href="#" className="item is-urgent">
+                    <span className="item-status">4</span>
+                    <h3 className="item-title">Microeconomics 101</h3>
+                    <p className="item-description">MIC101&nbsp;&nbsp;&bull;&nbsp;&nbsp;Ends on May 5, 2017</p>
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="item is-caution">
+                    <span className="item-status">1</span>
+                    <h3 className="item-title">Modern Entertainment &amp; Media Communication</h3>
+                    <p className="item-description">MEMC&nbsp;&nbsp;&bull;&nbsp;&nbsp;Ends on May 1, 2017</p>
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="item is-complete">
+                    <span className="item-status">0</span>
+                    <h3 className="item-title">Art History</h3>
+                    <p className="item-description">ARHI&nbsp;&nbsp;&bull;&nbsp;&nbsp;Ends on July 28, 2017</p>
+                  </a>
+                </li>
+              </ul>
               <h2 className="heading heading--s">Upcoming Courses</h2>
+              <ul className="has-items">
+                <li className="item item--disabled">
+                  <h3 className="item-title">Macroeconomics &amp; Corporate Finances</h3>
+                  <p className="item-description">Begins on August 20, 2017</p>
+                </li>
+              </ul>
             </main>
           </div>
         </div>
