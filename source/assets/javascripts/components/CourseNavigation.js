6fc3a2e0f888e62e5d552fff8071e31a2ee07a5c
John Wilson <john@j-wilson.com>
2016-12-23 19:27:59 -0500

    Finish basic styles for navigation

--- source/assets/javascripts/components/CourseNavigation.js
+++ source/assets/javascripts/components/CourseNavigation.js
@@ -4,11 +4,21 @@ import React from "react"
 class CourseNavigation extends React.Component {
   render() {
     return (
-      <header className="header">
-        <div className="cell well">
-          <div className="g">
+      <header className="row">
+        <div className="cell">
+          <div className="g g--center header">
             <div className="g-b g-b--3of12">
               <Logo />
+            </div>
+            <div className="header-items g-b g-b--9of12">
+              <h1 className="heading">Courses</h1>
+              <nav>
+                <a href="#" className="button button--heading"><span>3</span> Messages</a>
+                <a href="#" className="button button--heading button--outline">
+                  <img src="https://unsplash.it/50/50" />
+                  My Account
+                </a>
+              </nav>
             </div>
           </div>
         </div>
