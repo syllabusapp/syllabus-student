e5ce27119d0799dfe2ff0abf6760fe8e879b8a43
John Wilson <john@j-wilson.com>
2017-01-01 15:18:06 -0500

    Make content wrapper fixed position

--- source/assets/javascripts/application.js
+++ source/assets/javascripts/application.js
@@ -6,60 +6,58 @@ class AppFrame extends React.Component {
   render() {
     return (
       <div className="row">
-        <div className="cell">
-          <CourseNavigation />
-          <div className="g g--stretch">
-            <aside className="g-b g-b--3of12 g-b--spaceBetween">
-              <nav>
-                <a href="#" className="anchor">
-                  <span className="anchor-count">3</span>
-                  Courses
+        <CourseNavigation />
+        <div className="cell cell--content">
+          <aside className="sidebar">
+            <nav>
+              <a href="#" className="anchor">
+                <span className="anchor-count">3</span>
+                Courses
+              </a>
+              <a href="#" className="anchor is-alert">
+                <span className="anchor-count">1</span>
+                Documents
+              </a>
+            </nav>
+            <footer className="footer">
+              <a href="http://syllabusapp.com">Sign Out</a>
+              <a href="#">Privacy Policy</a>
+              <a href="#">Support</a>
+            </footer>
+          </aside>
+          <main className="content">
+            <h2 className="heading heading--s">My Current Courses</h2>
+            <ul className="has-items">
+              <li>
+                <a href="#" className="item is-urgent">
+                  <span className="item-status">4</span>
+                  <h3 className="item-title">Microeconomics 101</h3>
+                  <p className="item-description">MIC101&nbsp;&nbsp;&bull;&nbsp;&nbsp;Ends on May 5, 2017</p>
                 </a>
-                <a href="#" className="anchor is-alert">
-                  <span className="anchor-count">1</span>
-                  Documents
+              </li>
+              <li>
+                <a href="#" className="item is-caution">
+                  <span className="item-status">1</span>
+                  <h3 className="item-title">Modern Entertainment &amp; Media Communication</h3>
+                  <p className="item-description">MEMC&nbsp;&nbsp;&bull;&nbsp;&nbsp;Ends on May 1, 2017</p>
                 </a>
-              </nav>
-              <footer className="footer">
-                <a href="http://syllabusapp.com">Sign Out</a>
-                <a href="#">Privacy Policy</a>
-                <a href="#">Support</a>
-              </footer>
-            </aside>
-            <main className="g-b g-b--9of12">
-              <h2 className="heading heading--s">My Current Courses</h2>
-              <ul className="has-items">
-                <li>
-                  <a href="#" className="item is-urgent">
-                    <span className="item-status">4</span>
-                    <h3 className="item-title">Microeconomics 101</h3>
-                    <p className="item-description">MIC101&nbsp;&nbsp;&bull;&nbsp;&nbsp;Ends on May 5, 2017</p>
-                  </a>
-                </li>
-                <li>
-                  <a href="#" className="item is-caution">
-                    <span className="item-status">1</span>
-                    <h3 className="item-title">Modern Entertainment &amp; Media Communication</h3>
-                    <p className="item-description">MEMC&nbsp;&nbsp;&bull;&nbsp;&nbsp;Ends on May 1, 2017</p>
-                  </a>
-                </li>
-                <li>
-                  <a href="#" className="item is-complete">
-                    <span className="item-status">0</span>
-                    <h3 className="item-title">Art History</h3>
-                    <p className="item-description">ARHI&nbsp;&nbsp;&bull;&nbsp;&nbsp;Ends on July 28, 2017</p>
-                  </a>
-                </li>
-              </ul>
-              <h2 className="heading heading--s">Upcoming Courses</h2>
-              <ul className="has-items">
-                <li className="item item--disabled">
-                  <h3 className="item-title">Macroeconomics &amp; Corporate Finances</h3>
-                  <p className="item-description">Begins on August 20, 2017</p>
-                </li>
-              </ul>
-            </main>
-          </div>
+              </li>
+              <li>
+                <a href="#" className="item is-complete">
+                  <span className="item-status">0</span>
+                  <h3 className="item-title">Art History</h3>
+                  <p className="item-description">ARHI&nbsp;&nbsp;&bull;&nbsp;&nbsp;Ends on July 28, 2017</p>
+                </a>
+              </li>
+            </ul>
+            <h2 className="heading heading--s">Upcoming Courses</h2>
+            <ul className="has-items">
+              <li className="item item--disabled">
+                <h3 className="item-title">Macroeconomics &amp; Corporate Finances</h3>
+                <p className="item-description">Begins on August 20, 2017</p>
+              </li>
+            </ul>
+          </main>
         </div>
       </div>
     );
