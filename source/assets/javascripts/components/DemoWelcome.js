dcd75854d2beda3256b0b363939b722bb9e14092
John Wilson <john@j-wilson.com>
2017-01-09 16:54:12 -0500

    Improve card and list stylings and update welcome for official use

--- source/assets/javascripts/components/DemoWelcome.js
+++ source/assets/javascripts/components/DemoWelcome.js
@@ -14,12 +14,12 @@ export default class extends Component {
         <div className="modal-overlay"></div>
         <div className="modal-content">
           <p className="heading heading--l mb0">🎉</p>
-          <h1 className="heading heading--l mb1">Clickable things!</h1>
+          <h1 className="heading heading--l mb1">Syllabus Demo</h1>
           <p>Welcome to the prototype of the Syllabus platform! Here are a few things you might like to know before diving in:</p>
           <div className="card card--outline mb3">
             <div className="cell cell--s">
-              <ul className="list">
-                <li>This demo covers the main features of the student platform.</li>
+              <ul className="list list--underlined">
+                <li>This demo covers the main features of the student platform. Teacher and administrator demos coming soon!</li>
                 <li>All demo content and interactions are static. To start over, simply refresh your browser.</li>
                 <li>The Syllabus University is fictional, and the logo can be replaced with your school's logo.</li>
               </ul>
