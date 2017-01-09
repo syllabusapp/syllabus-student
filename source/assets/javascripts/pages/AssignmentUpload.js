8d5ba8517b2b142502b735a072e6b1205a31140b
John Wilson <john@j-wilson.com>
2017-01-09 16:04:51 -0500

    Upload interaction area clean up

--- source/assets/javascripts/pages/AssignmentUpload.js
+++ source/assets/javascripts/pages/AssignmentUpload.js
@@ -29,8 +29,8 @@ export default class extends Component {
             <div className="g-b g-b--5of12 mt5">
               <h2 className="heading heading--l">Upload File</h2>
               <p><span className="label">Reminder</span> Please upload a PDF document that does not exceed 8mb in&nbsp;size.</p>
-              <div className="card card--dashed card--outline">
-                <a href="#">Click or drag-n-drop to upload</a>
+              <div className="card card--outline card--upload">
+                <a href="#">Click or drag-n-drop here to upload</a>
               </div>
             </div>
           </div>
