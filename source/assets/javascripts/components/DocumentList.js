5b92a3da501ee0b1acd7002ffa28fb69b5909494
John Wilson <john@j-wilson.com>
2017-01-09 22:07:47 -0500

    Create document grid for dashboard

--- source/assets/javascripts/components/DocumentList.js
+++ source/assets/javascripts/components/DocumentList.js
@@ -9,7 +9,101 @@ export default class extends Component {
     return (
       <main className="content">
         <h2 className="heading heading--s">Courses</h2>
-        <h2 className="heading heading--s">School</h2>
+        <div className="g">
+          <div className="g-b g-b--4of12">
+            <Link to="/document" className="card card--document">
+              <div className="card-content">
+                <img src="/assets/images/icon-folder.svg" />
+              </div>
+              <div className="card-footer">
+                <p>Macroeconomics & Microeconomics</p>
+                <small>12 Files</small>
+              </div>
+            </Link>
+          </div>
+          <div className="g-b g-b--4of12">
+            <Link to="/document" className="card card--document">
+              <div className="card-content">
+                <img src="/assets/images/icon-folder.svg" />
+              </div>
+              <div className="card-footer">
+                <p>Macroeconomics & Microeconomics</p>
+                <small>12 Files</small>
+              </div>
+            </Link>
+          </div>
+          <div className="g-b g-b--4of12">
+            <Link to="/document" className="card card--document">
+              <div className="card-content">
+                <img src="/assets/images/icon-folder.svg" />
+              </div>
+              <div className="card-footer">
+                <p>Macroeconomics & Microeconomics</p>
+                <small>12 Files</small>
+              </div>
+            </Link>
+          </div>
+          <div className="g-b g-b--4of12">
+            <Link to="/document" className="card card--document">
+              <div className="card-content">
+                <img src="/assets/images/icon-folder-warning.svg" />
+              </div>
+              <div className="card-footer">
+                <p>Macroeconomics & Microeconomics</p>
+                <small>12 Files</small>
+              </div>
+            </Link>
+          </div>
+          <div className="g-b g-b--4of12">
+            <Link to="/document" className="card card--document">
+              <div className="card-content">
+                <img src="/assets/images/icon-folder.svg" />
+              </div>
+              <div className="card-footer">
+                <p>Macroeconomics & Microeconomics</p>
+                <small>12 Files</small>
+              </div>
+            </Link>
+          </div>
+        </div>
+        <div className="well well--l">
+          <h2 className="heading heading--s">School</h2>
+          <div className="g">
+            <div className="g-b g-b--4of12">
+              <Link to="/document" className="card card--document">
+                <div className="card-content">
+                  <img src="/assets/images/icon-folder.svg" />
+                </div>
+                <div className="card-footer">
+                  <p>Macroeconomics & Microeconomics</p>
+                  <small>12 Files</small>
+                </div>
+              </Link>
+            </div>
+            <div className="g-b g-b--4of12">
+              <Link to="/document" className="card card--document">
+                <div className="card-content">
+                  <img src="/assets/images/icon-folder-warning.svg" />
+                </div>
+                <div className="card-footer">
+                  <p>Macroeconomics & Microeconomics</p>
+                  <small>12 Files</small>
+                </div>
+              </Link>
+            </div>
+            <div className="g-b g-b--4of12">
+              <Link to="/document" className="card card--document">
+                <div className="card-content">
+                  <img src="/assets/images/icon-folder.svg" />
+                </div>
+                <div className="card-footer">
+                  <p>Macroeconomics & Microeconomics</p>
+                  <small>12 Files</small>
+                </div>
+              </Link>
+            </div>
+          </div>
+        </div>
       </main>
     )
   }
