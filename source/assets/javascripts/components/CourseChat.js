75d7a9bc63c8c79cf2cbbbbbe88e6bfac2041464
John Wilson <john@j-wilson.com>
2017-01-14 15:25:06 -0500

    Create placeholder content for messages chat modal

--- source/assets/javascripts/components/CourseChat.js
+++ source/assets/javascripts/components/CourseChat.js
@@ -17,8 +17,47 @@ export default class extends Component {
     return (
       <div className="modal modal--chat">
         <div onClick={this.toggleChat} className="modal-overlay"></div>
-        <div className="modal-content">
-          <h1 className="heading">Messages</h1>
+        <div className="chat modal-content">
+          <div className="chat-header">
+            <div>
+              <h1 className="heading"><span className="label label--number">3</span>Messages</h1>
+            </div>
+            <div>
+              <button className="chat-new">&times;</button>
+              <button onClick={this.toggleChat} className="chat-close">&times;</button>
+            </div>
+          </div>
+          <div className="chat-content">
+            <ul className="has-items">
+              <li>
+                <a href="#" className="item item--chat is-new">
+                  <img src="https://unsplash.it/100/100" className="item-status" />
+                  <h3 className="item-title">John Wilson</h3>
+                  <p className="item-description">
+                    Hey! Did you find our homework difficult for MICEC? I can't seem to get a grasp on
+                  </p>
+                </a>
+              </li>
+              <li>
+                <a href="#" className="item item--chat is-new">
+                  <img src="https://unsplash.it/100/100" className="item-status" />
+                  <h3 className="item-title">John Wilson</h3>
+                  <p className="item-description">
+                    Hey! Did you find our homework difficult for MICEC? I can't seem to get a grasp on
+                  </p>
+                </a>
+              </li>
+              <li>
+                <a href="#" className="item item--chat">
+                  <img src="https://unsplash.it/100/100" className="item-status" />
+                  <h3 className="item-title">John Wilson</h3>
+                  <p className="item-description">
+                    Hey! Did you find our homework difficult for MICEC? I can't seem to get a grasp on
+                  </p>
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     )
