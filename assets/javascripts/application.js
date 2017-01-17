1758b83ec84d28f99b07daccb86eabe84d52098b
John Wilson <john@j-wilson.com>
2017-01-16 21:10:13 -0500

    Update 2017-01-17T02:10:09.873Z

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -29004,7 +29004,12 @@
 	  function _class(props) {
 	    _classCallCheck(this, _class);
 
-	    return _possibleConstructorReturn(this, (_class.__proto__ || Object.getPrototypeOf(_class)).call(this, props));
+	    var _this = _possibleConstructorReturn(this, (_class.__proto__ || Object.getPrototypeOf(_class)).call(this, props));
+
+	    _this.state = {
+	      text: '<h2>Writing in Syllabus</h2><p>Writing a paper or critical thought response in Syllabus is super easy and enjoyable. Simply begin typing, then highlight the text that you want to format. This text you are reading is a sample paper, so go ahead and give it a try: highlight this text. Writing a paper within Syllabus allows a student to save their work then pick up where the left off on a completely different device.</p><h2>Style Guides</h2><p>Many teachers prefer to have students type papers in a specific format such as Turabian or Chicago Manual of Style. With Syllabus, the teacher can decide if they want to create an upload assignment and have their students upload a Word document with the correct format.</p>'
+	    };
+	    return _this;
 	  }
 
 	  _createClass(_class, [{
@@ -29114,10 +29119,11 @@
 	                    text: 'Begin typing your response here...'
 	                  },
 	                  toolbar: {
-	                    buttons: ['bold', 'italic', 'underline']
+	                    buttons: ['h1', 'h2', 'bold', 'italic', 'underline', 'quote']
 	                  }
 	                },
-	                tag: "article" })
+	                tag: "article",
+	                text: this.state.text })
 	            )
 	          )
 	        )
