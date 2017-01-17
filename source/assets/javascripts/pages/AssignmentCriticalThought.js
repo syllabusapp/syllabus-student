3750e882285be6aaeb7a9109b189c96b56b867e0
John Wilson <john@j-wilson.com>
2017-01-16 21:09:48 -0500

    Begin creating content for critical thought assignments

--- source/assets/javascripts/pages/AssignmentCriticalThought.js
+++ source/assets/javascripts/pages/AssignmentCriticalThought.js
@@ -7,6 +7,9 @@ import AssignmentNavigation from "../components/AssignmentNavigation"
 export default class extends Component {
   constructor(props) {
     super(props)
+    this.state = {
+      text: '<h2>Writing in Syllabus</h2><p>Writing a paper or critical thought response in Syllabus is super easy and enjoyable. Simply begin typing, then highlight the text that you want to format. This text you are reading is a sample paper, so go ahead and give it a try: highlight this text. Writing a paper within Syllabus allows a student to save their work then pick up where the left off on a completely different device.</p><h2>Style Guides</h2><p>Many teachers prefer to have students type papers in a specific format such as Turabian or Chicago Manual of Style. With Syllabus, the teacher can decide if they want to create an upload assignment and have their students upload a Word document with the correct format.</p>'
+    }
   }
   render() {
     return (
@@ -43,11 +46,12 @@ export default class extends Component {
                       text: 'Begin typing your response here...'
                     },
                     toolbar: {
-                      buttons: ['bold', 'italic', 'underline']
+                      buttons: ['h1', 'h2', 'bold', 'italic', 'underline', 'quote']
                     }
                   }
                 }
-                tag="article" />
+                tag="article"
+                text={this.state.text} />
             </div>
           </div>
         </div>
